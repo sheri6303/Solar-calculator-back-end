5908a3e1159cae9236d978efdca5021bca2bf869
sheri6303 <72887573+sheri6303@users.noreply.github.com>
2024-10-24 09:01:50 +0500

    Update google-sun-roof.service.ts

--- src/google-sun-roof/google-sun-roof.service.ts
+++ src/google-sun-roof/google-sun-roof.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GoogleSunRoofService {
     getHello(): string {
-        return 'Hello from google sun roof module!';
+        return 'Hello from google sun roof moduless!';
       }
 }
